b501e96b3d3afb93462c1848f8d601baf69781eb
llarreta <leonel@larreta.com.ar>
2021-05-15 12:25:09 -0300

    Ejemplo de iterador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewDemoComponent } from './view-demo/view-demo.component';
+import { IterateComponentComponent } from './iterate-component/iterate-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewDemoComponent
+    ViewDemoComponent,
+    IterateComponentComponent
   ],
   imports: [
     BrowserModule,
